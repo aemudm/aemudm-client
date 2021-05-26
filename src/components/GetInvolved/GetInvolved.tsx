6fee65a2c8a53d9253294d739b3ac71b2b6384e9
Bilal Itani <bitani@redcross.ca>
2021-05-26 09:13:23 -0400

    New URL for registration form.

--- src/components/GetInvolved/GetInvolved.tsx
+++ src/components/GetInvolved/GetInvolved.tsx
@@ -9,7 +9,7 @@ export const GetInvolved = (): JSX.Element => (
   <GetInvolvedStyled id='end'>
     <img src={images.background4} alt='background4' />
     <div className='content'>
-        <a target='_blank' rel='noopener noreferrer' href='https://docs.google.com/forms/d/e/1FAIpQLSc60scEU91BZabrTQl_hRQROGnsqC-W8RsfYhFpe4SOVYh05Q/viewform'>Devenir membre</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://docs.google.com/forms/d/e/1FAIpQLSd2j-2NgaFXlyUfOqen80-5BdqfhSwBCinEcf3H9SwGoTskQw/viewform'>Devenir membre</a>
         <a target='_blank' rel='noopener noreferrer' href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PEDJ5XEWXD7VJ&source=url'>Faire un don</a>
     </div>
     <div className='footer'>
