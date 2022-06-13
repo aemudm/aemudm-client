626af4dc5429c8986e744550cebaf68b9dc94dde
Bilal Itani <bilal.itani1234@gmail.com>
2022-06-13 08:46:59 -0400

    Security update + updated getinvolved url.

--- src/components/GetInvolved/GetInvolved.tsx
+++ src/components/GetInvolved/GetInvolved.tsx
@@ -9,8 +9,8 @@ export const GetInvolved = (): JSX.Element => (
   <GetInvolvedStyled id='end'>
     <img src={images.background4} alt='background4' />
     <div className='content'>
-        <a target='_blank' rel='noopener noreferrer' href='https://docs.google.com/forms/d/e/1FAIpQLSd2j-2NgaFXlyUfOqen80-5BdqfhSwBCinEcf3H9SwGoTskQw/viewform'>Devenir membre</a>
-        <a target='_blank' rel='noopener noreferrer' href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PEDJ5XEWXD7VJ&source=url'>Faire un don</a>
+      <a target='_blank' rel='noopener noreferrer' href='https://forms.gle/qi3WyUoGhTP3jTht9'>Devenir membre</a>
+      <a target='_blank' rel='noopener noreferrer' href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PEDJ5XEWXD7VJ&source=url'>Faire un don</a>
     </div>
     <div className='footer'>
       <img src={images.baniere} alt='baniere' />
